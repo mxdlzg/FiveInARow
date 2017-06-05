aa10874a7898283f6cfa56d6504ea22b443f5b18
mxdlzg <mxdlzg@163.com>
2017-06-05 22:26:39 +0800

    添加五元组评分方法

--- src/util/ChessPosRound.java
+++ src/util/ChessPosRound.java
@@ -1,5 +1,7 @@
 package util;
 
+import entity.Config;
+
 /**
  * Created by 廷江 on 2017/6/4.
  */
@@ -13,5 +15,61 @@ public class ChessPosRound {
     public static int posRound(int value){
         int count = value/50;
         return (int) (count*49+30);
+    }
+
+    /**
+     * @param chesses 5元祖
+     * @return 评分
+     */
+    public static int rankScore(Integer...chesses){
+        int whiteCount = 0;
+        int blackCount = 0;
+        int score = 0;
+        boolean isBlack = false;
+        for (int i = 0;i<5;i++){
+            if (chesses[i] == Config.COLOR_WHITE){
+                whiteCount++;
+            }else if (chesses[i]==Config.COLOR_BLACK){
+                blackCount++;
+            }
+        }
+        if (whiteCount != 0 && blackCount!=0){
+            return 0;
+        }
+        if (whiteCount == 0 && blackCount == 0){
+            return 7;
+        }
+        isBlack = whiteCount == 0;
+        switch (isBlack?blackCount:whiteCount){
+            case 1:
+                if (isBlack){
+                    score = 35;
+                }else {
+                    score = 15;
+                }
+                break;
+            case 2:
+                if (isBlack){
+                    score = 800;
+                }else {
+                    score = 400;
+                }
+                break;
+            case 3:
+                if (isBlack){
+                    score = 15000;
+                }else {
+                    score = 1800;
+                }
+                break;
+            case 4:
+                if (isBlack){
+                    score = 800000;
+                }else {
+                    score = 100000;
+                }
+                break;
+        }
+        return score;
     }
 }

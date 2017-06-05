1dbb844115159bd3137feca8d93902d7c33eabbd
mxdlzg <mxdlzg@163.com>
2017-06-05 22:27:43 +0800

    添加构造方法

--- src/control/VictoryInspector.java
+++ src/control/VictoryInspector.java
@@ -2,22 +2,22 @@ package control;
 
 import entity.Chessman;
 
-import java.awt.*;
 import java.util.List;
 
 /**
  * Created by 廷江 on 2017/6/3.
  */
 public class VictoryInspector {
-    private Color userColor;
+    private int userColor;
+    private int[][] chessboardArray;
     List<Chessman> chessmanList;
 
     public VictoryInspector() {
     }
 
-    public VictoryInspector(Color userColor, List<Chessman> chessmanList) {
+    public VictoryInspector(int userColor, int[][] chessboardArray) {
         this.userColor = userColor;
-        this.chessmanList = chessmanList;
+        this.chessboardArray = chessboardArray;
     }
 
 

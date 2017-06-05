43c072b2ef0076d21bf0325a47d774dfac60aca8
mxdlzg <mxdlzg@163.com>
2017-06-05 22:25:32 +0800

    添加用户动作和系统动作，更改用户下落函数 单独抽出绘图功能组成新的函数 添加用户最后一步棋跟踪 添加轮转调度

--- src/ui/ChessboardControl.java
+++ src/ui/ChessboardControl.java
@@ -2,11 +2,13 @@ package ui;
 
 import control.Machine;
 import control.VictoryInspector;
+import entity.Chessman;
 import entity.Config;
 import util.ChessPosRound;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 /**
  * Created by 廷江 on 2017/6/4.
@@ -16,11 +18,17 @@ public class ChessboardControl {
     private JPanel chessboardPanel;
     private JPanel controlPanel;
     private JLabel background;
-    private int userColor;
+    private int userColor = Config.COLOR_WHITE;
+    private int machineColor = Config.COLOR_BLACK;
+    private boolean isUserTurn = true;
+
+    private Chessman[][] maxRect;    //00左上 01右上 10左下 11右下
 
     private int[][] chessboardArray ;
     private VictoryInspector victoryInspector;
     private Machine machine;
+
+    private Chessman lastPosition;
 
     /**
      * 暂时无用
@@ -47,9 +55,59 @@ public class ChessboardControl {
      * 初始化控制器用到的参数和对象
      */
     private void init(){
-        chessboardArray = new int[15][15];
+        maxRect = new Chessman[2][2];
+        lastPosition = new Chessman();
+        chessboardArray = new int[16][16];
         victoryInspector = new VictoryInspector();
-        machine = new Machine();
+        machine = new Machine(chessboardArray);
+    }
+
+    /**
+     * 重设棋局
+     */
+    public void reset(){
+
+    }
+
+    public void initUserColor(){
+        Random random = new Random();
+    }
+
+    public void changeOperator(){
+
+    }
+
+    /**
+     * 机器下子
+     */
+    public void machineAction(){
+        if (!isUserTurn){
+            System.out.println("机器下子");
+
+        }
+        isUserTurn = true;
+    }
+
+    /**
+     * 用户下子
+     * @param x x
+     * @param y y
+     */
+    public void userAction(int x,int y){
+        if (isUserTurn && posIsAllowed(x,y)){
+            System.out.println("用户下子");
+            onActionDown(x,y);
+            lastPosition.setColor(userColor);
+            lastPosition.setPosX(x/50);
+            lastPosition.setPosY(y/50);
+            // TODO: 2017/6/5 在这里对用户行为进行victory检查
+
+            
+            isUserTurn = false;
+            // TODO: 2017/6/5 用户下子后，启动machine线程，对现场进行判断，给出机器下子位置
+            machineAction();
+            // TODO: 2017/6/5 在这里对机器行为进行victory检查
+        }
     }
 
     /**
@@ -57,19 +115,75 @@ public class ChessboardControl {
      * @param x 横坐标
      * @param y 纵坐标
      */
-    void onActionDown(int x, int y){
+    private void onActionDown(int x, int y){
+        drawChessman(x,y,userColor);
+        chessboardArray[x/50][y/50] = userColor;
+    }
+
+    /**
+     * 绘制棋子
+     * @param x x
+     * @param y y
+     * @param color 颜色
+     */
+    private void drawChessman(int x,int y,int color){
         Graphics graphics = background.getGraphics();
-        if (userColor == Config.COLOR_BLACK){
-            graphics.setColor(Color.BLACK);
-        }else {
+        if (color == Config.COLOR_WHITE){
             graphics.setColor(Color.WHITE);
+        }else {
+            graphics.setColor(Color.BLACK);
         }
-        chessboardArray[x/50][y/50] = 1;
         x = ChessPosRound.posRound(x);
         y = ChessPosRound.posRound(y);
         graphics.fillOval(x - 15, y - 15, 30, 30);
     }
 
+    /**
+     * @param x x
+     * @param y y
+     * @return 当前位置是否允许下子
+     */
+    private boolean posIsAllowed(int x,int y){
+        return chessboardArray[x / 50][y / 50] == 0;
+    }
+
+    /**
+     * 检查最大边界，每添加一个新的棋子就检查一次
+     * @param x x
+     * @param y y
+     */
+    private void checkMaxRect(int x,int y){
+        // TODO: 2017/6/5 最大边界检查 
+        x = x/50;
+        y = y/50;
+        if (x>maxRect[1][1].getPosX()&& y>maxRect[1][1].getPosY()){
+            maxRect[1][1].setPosX(x);
+            maxRect[1][1].setPosY(y);
+        }
+    }
+
+    /**
+     * 棋局结束之后对ui进行更新和提示胜利
+     * @param color 胜利者的棋子颜色
+     */
+    private void victoryAction(int color){
+
+    }
+
+
+    /**
+     * @return 玩家最后一步棋的位置
+     */
+    public Chessman getLastPosition() {
+        return lastPosition;
+    }
+
+    /**
+     * @param lastPosition 最后一步棋
+     */
+    public void setLastPosition(Chessman lastPosition) {
+        this.lastPosition = lastPosition;
+    }
 
     /**
      * @return 用户当前使用的棋子颜色
@@ -85,4 +199,17 @@ public class ChessboardControl {
         this.userColor = userColor;
     }
 
+    /**
+     * @return 当前是否为用户操作
+     */
+    public boolean isUserTurn() {
+        return isUserTurn;
+    }
+
+    /**
+     * @param userTurn 当前是否为用户操作
+     */
+    public void setUserTurn(boolean userTurn) {
+        isUserTurn = userTurn;
+    }
 }
